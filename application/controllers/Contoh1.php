fdd12f52e81636f5e97605fcf49e459eea168411
adityafernanda76 <adityafernanda76@gmail.com>
2023-09-26 20:46:57 +0700

    Membuat project CI 3 Pustaka Booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index(){
+        echo "<h1> Perkenalan </h1>";
+        echo "Nama saya Aditya Fernanda <br>
+        Saya tinggal di daerah Cengkareng <br>
+        Olah raga yang saya sukai adalah Renang";
+    }
+
+}
+
+?>
